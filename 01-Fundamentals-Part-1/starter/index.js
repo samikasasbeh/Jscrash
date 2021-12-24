d555dc873c074f63ee2b44ad39210a8d6bb45cb0
Sami Kasasbeh <Sami.Kasasbehdev@gmail.com>
2021-12-24 03:31:00 -0330

    testing with variables

--- 01-Fundamentals-Part-1/starter/index.js
+++ 01-Fundamentals-Part-1/starter/index.js
@@ -1,2 +1,15 @@
 let js = "amazing";
 if (js === "amazing") alert("Statment is true!");
+
+let test1 = 23
+test1 = 1
+console.log(test1)
+console.log(typeof test1)
+let test2 = "string"
+test2 = 1
+console.log(test2)
+console.log(typeof test2)
+let test3;
+console.log(test3)
+console.log(typeof test3)
+
